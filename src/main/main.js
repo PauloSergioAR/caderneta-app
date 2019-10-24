3d4976a9d695271604828fa40633b501fbb9b30b
Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>
2019-10-23 21:28:10 -0300

    Salvando no firebase
    Signed-off-by: Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>

--- src/main/main.js
+++ src/main/main.js
@@ -1,30 +1,170 @@
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { StyleSheet, Platform, Image, Text, View, ActivityIndicator, FlatList, Dimensions, Picker } from 'react-native'
 import firebase from 'react-native-firebase';
+import { ListItem, Overlay, Button, Input } from 'react-native-elements';
+
+import DatePicker from 'react-native-datepicker'
+
+import OverlayComponent from './overlay'
+
+import * as IconEntypo from 'react-native-vector-icons/Entypo'
+import * as IconFontAwesome from 'react-native-vector-icons/FontAwesome5'
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
+
 export default class Main extends React.Component {
-  state = { currentUser: null }
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null,
+      data: new Date().getDate,
+      visible: false,
+      name: '',
+      description: '',              
+      expire: '',
+      selectedValue: '0',
+      dateChanged: false
+    }
+    this.ref = firebase.firestore().collection('users')
+    this.ref.where('email', "==", firebase.auth().currentUser.email).get().then(snap => {
+      snap.forEach(doc => {
+        this.userRef = doc
+      })
+    })
+    this.update = this.update.bind(this)
+  }
+
 
   componentDidMount() {
     const { currentUser } = firebase.auth()
     this.setState({ currentUser })
+    this.update()
+  }
+
+  update = () => {
+    let data
+    firebase.firestore().collection('users')
+      .where("email", "==", firebase.auth().currentUser.email).get().then((snap) => {
+        snap.forEach((doc) => {
+          data = doc.data()
+          this.setState({
+            data: data
+          })
+        })
+      })
+  }
+  renderItem = ({ item }) => {
+    return (
+      <ListItem
+        style={{ alignSelf: "stretch" }}
+        title={item.descricao}
+        subtitle={item.valor}
+        bottomDivider
+      />
+    )
+
+  }
+
+  close() {
+    this.setState({
+      visible: false,
+    })
+  }
+
+  open() {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleComboboxChangeOption(val) {
+    if (val !== '0') {
+      this.setState({selectedValue: val});
+    }
+  }
+
+  modalCallback(data){
+    console.log(data)
+    this.setState({
+      visible: false
+    })
+    if(!this.userRef.data().debitos){
+      let debArray = []
+      debArray.push(data)
+      this.userRef.ref.update({
+        debitos: debArray
+      }).then(() => {
+        console.log("Added successfully")
+        this.update()
+    })
+      .catch(e => console.log("ERROR: ", e))      
+    } else{
+      let debArray = [ ]
+      debArray = this.userRef.data().debitos
+      debArray.push(data)
+      this.userRef.ref.update({
+        debitos: debArray
+      }).then(() => {
+        console.log("Added successfully")
+        this.update()
+      })
+      .catch(e => console.log("ERROR: ", e))
+    }
   }
 
   render() {
     const { currentUser } = this.state
 
+    let list = this.state.data ?
+      <FlatList
+        data={this.state.data.debitos}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={this.renderItem}
+      />
+      : <ActivityIndicator size="large" />
+
     return (
-      <View style={styles.container}>
-        <Text>
-          Hi {currentUser && currentUser.email}!
-        </Text>
+      <View style={styles.master}>
+        <OverlayComponent 
+          callback={this.modalCallback.bind(this)}
+          visible={this.state.visible}    
+        />
+        <View style={styles.container}>
+          <Text>
+            Hi {currentUser && currentUser.email}!
+          </Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Button onPress={this.open.bind(this)} 
+            icon={{
+              type: 'font-awesome', 
+              name: 'plus-circle',
+              color: "blue"
+            }}
+          />
+          {list}
+        </View>
       </View>
     )
   }
 }
 const styles = StyleSheet.create({
+  master: {
+    flex: 1,
+    alignSelf: 'center',
+    flexDirection: 'column'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    alignSelf: 'flex-start',
+    alignItems: 'center',
+  },
+
+  listContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'column-reverse',
+    width: width * .95,
   }
 })
---
+++ src/main/overlay.js
@@ -0,0 +1,117 @@
+import React, { useState, useCallback } from 'react';
+
+import { View, Switch, Dimensions, Picker } from 'react-native';
+
+import { ListItem, Overlay, Button, Input } from 'react-native-elements';
+import DatePicker from 'react-native-datepicker'
+
+// import { Container } from './styles';
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
+
+var data={ }
+
+const OverlayComponent = (props) => {  
+  const [descricao, setDescricao] = useState('')
+  const [date, setDate] = useState(new Date().getDate())
+  const [combo, setCombo] = useState('0')
+  const [categoria, setCategoria] = useState('')
+  const [valor, setValor] = useState('')
+
+  const onDescChange = useCallback(e => {
+    setDescricao(e)
+  })
+
+  const onDateChange = useCallback(e => {
+    setDate(e)
+  })
+
+  const onComboChange = useCallback(e => {
+    if(e != '0'){
+      setCombo(e)      
+    }
+  })
+
+  const onCatChange = useCallback(e => {
+    setCategoria(e)
+  })
+
+  const onValChange = useCallback(e => {
+    setValor(e)
+  })
+
+  const handleSubmit = () => {
+    let data = {
+      descricao: descricao,
+      date: date,
+      tipo: combo,
+      categoria: categoria,
+      valor: valor
+    }
+    props.callback(data)
+    data = null
+    setCategoria('')
+    setCombo('0')
+    setDate(new Date().getDate())
+    setDescricao('')
+    setValor('')
+  }
+
+  return(
+    <View>
+      <Overlay
+          isVisible={props.visible}
+          width={width * .8}
+          height={height * .8}
+        >
+          <Input
+            placeholder="Descrição"
+            onChangeText={(text) => onDescChange(text)}            
+          />
+          <DatePicker
+            style={{width: width * .73}}
+            date={data.date}
+            mode="date"
+            placeholder={data.date}
+            format="DD-MM-YYYY"            
+            confirmBtnText="Confirmar"
+            cancelBtnText="Cancelar"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }          
+            }}
+            onDateChange={(d) => onDateChange(d)}
+          />
+          <Picker
+            selectedValue={combo}
+            onValueChange={onComboChange}
+          >
+            <Picker.Item label="..." value='0'/>
+            <Picker.Item label="A Pagar" value="pagar"/>
+            <Picker.Item label="A Receber" value="receber"/>
+          </Picker>
+          <Input
+            placeholder="Categoria"
+            onChangeText={(text) => onCatChange(text)}            
+          />          
+          <Input
+            placeholder="Valor"
+            onChangeText={(text) => onValChange(text)}
+            name="val"
+          />          
+          <Button onPress={handleSubmit.bind(this)} title="Close"></Button>
+        </Overlay>
+    </View>
+
+  )
+}
+
+export default OverlayComponent;
